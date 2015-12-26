85e824e1f707a500138b07c7ef89d30a4f028866
LamboAltisLife <ross.a500.acer@gmail.com>
2015-12-26 22:43:20 +0000

    Made Changes to Master
    Made sell price 0 & changed Life_Settings

--- Altis_Life.Altis/Config_Master.hpp
+++ Altis_Life.Altis/Config_Master.hpp
@@ -28,11 +28,11 @@
 */
 class Life_Settings {
 	/* Persistent Settings */
-	save_civ_weapons = false; //Allow civilians to save weapons on them?
+	save_civ_weapons = true; //Allow civilians to save weapons on them?
 	save_virtualItems = true; //Save Virtual items (all sides)?
 
 	/* Revive system settings */
-	revive_cops = true; //true to enable cops the ability to revive everyone or false for only medics/ems.
+	revive_cops = false; //true to enable cops the ability to revive everyone or false for only medics/ems.
 	revive_fee = 1500; //Revive fee that players have to pay and medics / EMS are rewarded
 	
 	/* House related settings */
@@ -50,12 +50,12 @@ class Life_Settings {
 	paycheck_period = 5; //Scaled in minutes
 	
 	/* Impound Variables */
-	impound_car = 350; //Price for impounding cars
-	impound_boat = 250; //Price for impounding boats
-	impound_air = 850; //Price for impounding helicopters / planes
+	impound_car = 3500; //Price for impounding cars
+	impound_boat = 2500; //Price for impounding boats
+	impound_air = 8500; //Price for impounding helicopters / planes
 
 	/* Car-shop Settings */
-	vehicleShop_rentalOnly[] = { "B_MRAP_01_hmg_F", "B_G_Offroad_01_armed_F" };
+	vehicleShop_rentalOnly[] = { "B_G_Offroad_01_armed_F" };
 
 	/* Job-related stuff */
 	delivery_points[] = { "dp_1", "dp_2", "dp_3", "dp_4", "dp_5", "dp_6", "dp_7", "dp_8", "dp_9", "dp_10", "dp_11", "dp_12", "dp_13", "dp_14", "dp_15", "dp_15", "dp_16", "dp_17", "dp_18", "dp_19", "dp_20", "dp_21", "dp_22", "dp_23", "dp_24", "dp_25" };
@@ -71,46 +71,59 @@ class Life_Settings {
 	};
 	
 	sellArray[] = {
-		{"arifle_sdar_F", 7500},
-		{"hgun_P07_snds_F", 650},
-		{"hgun_P07_F", 1500},
-		{"ItemGPS", 45},
-		{"ToolKit", 75},
-		{"FirstAidKit", 65},
-		{"Medikit", 450},
-		{"NVGoggles", 980},
-		{"16Rnd_9x21_Mag", 15},
-		{"20Rnd_556x45_UW_mag", 35},
-		{"ItemMap", 35},
-		{"ItemCompass", 25},
-		{"Chemlight_blue", 50},
-		{"Chemlight_yellow", 50},
-		{"Chemlight_green", 50},
-		{"Chemlight_red", 50},
-		{"hgun_Rook40_F", 500},
-		{"arifle_Katiba_F", 5000},
-		{"30Rnd_556x45_Stanag", 65},
-		{"20Rnd_762x51_Mag", 85},
-		{"30Rnd_65x39_caseless_green", 50},
-		{"DemoCharge_Remote_Mag", 7500},
-		{"SLAMDirectionalMine_Wire_Mag", 2575},
-		{"optic_ACO_grn", 250},
-		{"acc_flashlight", 100},
-		{"srifle_EBR_F", 15000},
-		{"arifle_TRG21_F", 3500},
-		{"optic_MRCO", 5000},
-		{"optic_Aco", 850},
-		{"arifle_MX_F", 7500},
-		{"arifle_MXC_F", 5000},
-		{"arifle_MXM_F", 8500},
-		{"MineDetector", 500},
-		{"optic_Holosight", 275},
-		{"acc_pointer_IR", 175},
-		{"arifle_TRG20_F", 2500},
-		{"SMG_01_F", 1500},
-		{"arifle_Mk20C_F", 4500},
-		{"30Rnd_45ACP_Mag_SMG_01", 60},
-		{"30Rnd_9x21_Mag", 30}
+		{"arifle_sdar_F", 0},
+		{"hgun_P07_snds_F", 0},
+		{"hgun_P07_F", 0},
+		{"ItemGPS", 0},
+		{"ToolKit", 0},
+		{"FirstAidKit", 0},
+		{"Medikit", 0},
+		{"NVGoggles", 0},
+		{"16Rnd_9x21_Mag", 0},
+		{"20Rnd_556x45_UW_mag", 0},
+		{"ItemMap", 0},
+		{"ItemCompass", 0},
+		{"Chemlight_blue", 0},
+		{"Chemlight_yellow", 0},
+		{"Chemlight_green", 0},
+		{"Chemlight_red", 0},
+		{"hgun_Rook40_F", 0},
+		{"arifle_Katiba_F", 0},
+		{"30Rnd_556x45_Stanag", 0},
+		{"20Rnd_762x51_Mag", 0},
+		{"30Rnd_65x39_caseless_green", 0},
+		{"DemoCharge_Remote_Mag", 0},
+		{"SLAMDirectionalMine_Wire_Mag", 0},
+		{"optic_ACO_grn", 0},
+		{"acc_flashlight", 0},
+		{"srifle_EBR_F", 0},
+		{"arifle_TRG21_F", 0},
+		{"optic_MRCO", 0},
+		{"optic_Aco", 0},
+		{"arifle_MX_F", 0},
+		{"arifle_MXC_F", 0},
+		{"arifle_MXM_F", 0},
+		{"MineDetector", 0},
+		{"optic_Holosight", 0},
+		{"acc_pointer_IR", 0},
+		{"arifle_TRG20_F", 0},
+		{"SMG_01_F", 0},
+		{"arifle_Mk20C_F", 0},
+		{"30Rnd_45ACP_Mag_SMG_01", 0},
+		{"hlc_rifle_ak12", 0},
+		{"hlc_rifle_ak47", 0},
+		{"hlc_rifle_ak74", 0},
+		{"hlc_rifle_akm", 0},
+		{"hlc_rifle_aks74", 0},
+		{"hlc_rifle_aks74u", 0},
+		{"hlc_rifle_rpk", 0},
+		{"hlc_optic_kobra", 0},
+		{"hlc_30Rnd_545x39_B_AK", 0},
+		{"hlc_30Rnd_762x39_b_ak", 0},
+		{"hlc_75Rnd_762x39_m_rpk", 0},
+		{"30Rnd_9x21_Mag", 0}
+		
+		
 	};
 
 	allowedSavedVirtualItems[] = { "pickaxe", "fuelEmpty", "fuelFull", "spikeStrip", "lockpick", "defuseKit", "storageSmall", "storageBig", "redgull", "coffee", "waterBottle", "apple", "peach", "tbacon", "donut", "rabbitGrilled", "salemaGrilled", "ornateGrilled", "mackerelGrilled", "tunaGrilled", "mulletGrilled", "catsharkGrilled", "turtleSoup", "henGrilled", "roosterGrilled", "sheepGrilled", "goatGrilled" };
